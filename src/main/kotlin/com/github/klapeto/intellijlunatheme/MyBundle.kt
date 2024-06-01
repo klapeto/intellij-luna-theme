6c142c7b980d4e0e39327d278dc60e4cbf7e6108
GitHub Action <action@github.com>
2024-06-01 10:34:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/klapeto/intellijlunatheme/MyBundle.kt
+++ src/main/kotlin/com/github/klapeto/intellijlunatheme/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.klapeto.intellijlunatheme
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/klapeto/intellijlunatheme/services/MyProjectService.kt
+++ src/main/kotlin/com/github/klapeto/intellijlunatheme/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.klapeto.intellijlunatheme.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.klapeto.intellijlunatheme.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
